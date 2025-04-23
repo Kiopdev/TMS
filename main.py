4db673a033ac25a43a78f5034ca4888c273182e9
Kiopdev <160397126+Kiopdev@users.noreply.github.com>
2025-04-23 21:37:27 +0545

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import random
 import os
-bot.run(os.getenv(\"BOT_TOKEN\"))
+bot.run(os.getenv("BOT_TOKEN"))
 
 intents = discord.Intents.default()
 intents.message_content = True
